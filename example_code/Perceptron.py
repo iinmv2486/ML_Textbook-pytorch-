f79ced409e75e842dc420a173a3ba039de7b9aab
iinmv2486 <iinmv2486@gmail.com>
2024-09-30 00:26:34 +0900

    09/30

--- example_code/Perceptron.py
+++ example_code/Perceptron.py
@@ -1,48 +1,18 @@
 import numpy as np
 
 class Perceptron:
-    """퍼셉트론 분류기
-    
-    매개변수
-    ----------
-    eta : float
-        학습률 (0.0과 1.0 사이)
-    n_iter : int
-        훈련 데이터셋 반복 횟수
-    random_state : int
-        가중치 무작위 초기화를 위한 난수 생성기 시드
-
-    속성
-    ----------
-    w_ : 1d-array
-        학습된 가중치
-    b_ : 스칼라
-        학습된 절편 유닛
-
-    errors_ : list
-        에포크마다 누적된 분류 오류 
-    """
-
     def __init__(self, eta=0.01, n_iter=50, random_state=1) -> None:
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
     
+    def net_input(self, X):
+        return np.dot(X, self.w_) + self.b_
+    
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, 0)  # 입력된 계산이 0보다 크면 1, 작으면 0
+    
     def fit(self, X, y):
-        """훈련 데이터 학습
-        
-        매개변수
-        ----------
-        X : {array-like}, shape = [n_samples, n_features]
-            n_samples개의 샘플과 n_features개의 특성으로 이루어진 훈련 데이터
-        y : array-like, shape = [n_samples]
-            타깃 값
-        
-        반환값
-        ----------
-        self : object
-
-        """
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
         self.b_ = np.float_(0.)
@@ -57,15 +27,3 @@ class Perceptron:
                 errors += int(update != 0.0)
             self.errors_.append(errors)
         return self
-    
-    def net_input(self, X):
-        """입력 계산"""
-        return np.dot(X, self.w_) + self.b_
-    
-    def predict(self, X):
-        """단위 계단 함수를 사용하여 클래스 레이블을 반환한다."""
-        return np.where(self.net_input(X) >= 0.0, 1.0)
-
-if __name__ == "__main__":
-    a = -1.0
-    print(bool(a != 0.0))
